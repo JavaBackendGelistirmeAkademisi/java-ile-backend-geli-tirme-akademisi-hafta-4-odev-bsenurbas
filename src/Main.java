663f49562ec1a8d9d665f089b31e5631aff57b75
Buse Nur Bas <bsenurbas@outlook.com>
2024-09-12 13:40:04 +0300

    Ödev Tamamlandı

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        productManagement productManagement = new productManagement();
+
+        try {
+            productManagement.loadProducts();
+        } catch (IOException e) {
+            System.out.println("Error loading products" + e.getMessage());
+        }
+
+        Product newProduct = new Product("Laptop",10,15000);
+        productManagement.addProduct(newProduct);
+
+        Customer customer = new Customer("Buse Nur", "busenur@example.com");
+        Order order = new Order(customer);
+        order.addProduct(newProduct);
+        order.saveOrderToFile();
+
+        try{
+            productManagement.updateStock("Laptop", 8);
+        }catch (ProductNotFoundException e){
+            System.out.println("Product not found" + e.getMessage());
+        }
     }
 }
